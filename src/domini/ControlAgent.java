a17eb04c2987910a17b0de6c8f4b3407c2f00ac6
Joan Pagès Esbri <joan@HP-elem.(none)>
2014-10-29 17:56:47 +0100

    modified:   bin/domini/ControlAgent.class 	modified:   src/domini/ControlAgent.java

--- src/domini/ControlAgent.java
+++ src/domini/ControlAgent.java
@@ -1,8 +1,64 @@
 package domini;
 
+import java.util.*;
 import java.util.ArrayList;
+
+import static java.util.Collections.binarySearch;
 
 public class ControlAgent {
 
     private ArrayList<Agent> Agents;
+    private int nAgents;
+    
+    public ControlAgent(){
+    	Agents = new ArrayList();
+    	nAgents = 0;
+    }
+    
+    public ControlAgent(int nouID){
+	/*
+	 * 
+	 */
+    }
+    
+    public void nouAgent(int nouID){
+    	Agent a = new Agent(nouID);
+    	Agents.add(a);
+    	++nAgents;
+    }
+    
+    public void eliminaAgent(int idAgent){
+    	for(int i = 0 ; i<nAgents ; ++i){
+    		/*
+    		 * import java.util.List;
+				...
+				List<String> l = arrayList;
+				for( String s : l ){
+			  		System.out.println( s );
+				}
+    		 */
+    	}
+    }
+    public Agent getAgent(int id){
+    	return null;
+    }
+    
+    public void modificaItinerari(Agent a, Itinerari it){
+    	
+    }
+    public void modificaReunio(Agent a, Reunio re){
+		
+	}
+    
+    
+    public void eliminarItinerari(Agent a, Itinerari it){
+    	
+    }
+    public void eliminarReunio(Agent a, Reunio re){
+		
+	}
+    
+    
+    
+    
 }

2f861cf4c0988261f8154850e04d6414c6846caa
susanhenr <susan.henr@gmail.com>
2014-11-06 12:40:17 +0100

    modifiquéestomuehehe

--- src/domini/Itinerari.java
+++ src/domini/Itinerari.java
@@ -1,11 +1,27 @@
+
 package domini;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author alumne
+ * @author eric
  */
 public class Itinerari extends Cami{
 	
-	Agent agent;
-      
+	public List<Agent> llistatAgents = new ArrayList<Agent>();
+
+    public Itinerari(int id) {
+        super(id);
+    }
+
+
+        public void insertAgent(Agent a){
+            this.llistatAgents.add(a);
+        }
+        
+        public List<Agent> getAgent(){
+            return this.llistatAgents;
+        }
 }
